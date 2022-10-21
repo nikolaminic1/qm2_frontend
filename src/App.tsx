e8429af6635dc8db0282c3cf96a238582e97fd8c
minicnikola1 <minicnikola999@gmail.com>
2022-10-21 23:22:39 +0200

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,56 +1,44 @@
-import React from 'react';
-import logo from './logo.svg';
-import { Counter } from './features/counter/Counter';
-import './App.css';
+import React from "react";
+import "./App.css";
+import { CookiesProvider } from "react-cookie";
+import {
+  BrowserRouter,
+  Routes,
+  Route,
+  Link,
+} from "react-router-dom";
+import ErrorBoundary from "antd/lib/alert/ErrorBoundary";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <Counter />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <span>
-          <span>Learn </span>
-          <a
-            className="App-link"
-            href="https://reactjs.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            React
-          </a>
-          <span>, </span>
-          <a
-            className="App-link"
-            href="https://redux.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Redux
-          </a>
-          <span>, </span>
-          <a
-            className="App-link"
-            href="https://redux-toolkit.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Redux Toolkit
-          </a>
-          ,<span> and </span>
-          <a
-            className="App-link"
-            href="https://react-redux.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            React Redux
-          </a>
-        </span>
-      </header>
+      <ErrorBoundary>
+        <CookiesProvider>
+          <BrowserRouter>
+            <CLayout>
+              <Routes>
+                <Route exact path="/" element={<Home/>}/>
+          
+                <Route path="users/*" element={<Users />} />
+
+                <Route path={"*"} element={<NotFound/>}>
+            
+              </Switch>
+            </CLayout>
+          </BrowserRouter>
+        </CookiesProvider>
+      </ErrorBoundary>
+    </div>
+  );
+}
+
+function Users() {
+  return (
+    <div>
+      <Routes>
+        <Route path=":id" element={<UserProfile />} />
+        <Route path="me" element={<OwnUserProfile />} />
+      </Routes>
     </div>
   );
 }
